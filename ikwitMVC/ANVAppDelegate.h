8d8ede82386aba4423caa93628ad97a15e1e595c
Andrei Nechaev <faradey@andreis-mbp.home>
2014-07-07 22:13:56 -0400

    Initial Commit

---
+++ ikwitMVC/ANVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANVAppDelegate.h
+//  ikwitMVC
+//
+//  Created by Andrei Nechaev on 7/7/14.
+//  Copyright (c) 2014 Andrei Nechaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ikwitMVC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ikwitMVC
+//
+//  Created by Andrei Nechaev on 7/7/14.
+//  Copyright (c) 2014 Andrei Nechaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ANVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ANVAppDelegate class]));
+    }
+}
